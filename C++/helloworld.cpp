3b1e0ebb3b45f3e7a38662b91bf4811107b04922
yodayeong <duekdud01@naver.com>
2023-03-09 13:58:27 +0900

    feat: c++ 기본출력 공부

---
+++ C++/helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //헤더파일
+
+using namespace std;  //네임스페이스
+
+int main(void) {
+    cout << "Hello World" << endl; //문자열 출력
+
+    return 0;
+}
---
+++ C++/practice.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << "This is practice.\n"
+    "My name is Dayeong!" << endl;
+
+    return 0;
+}
