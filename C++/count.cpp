798265e4ce7d9a405ed3e6d9f10eeae16879cb49
yodayeong <duekdud01@naver.com>
2023-03-09 14:09:28 +0900

    feat: c++ 변수 공부

---
+++ C++/count.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    int c;
+
+    a = 100;
+    b = 200;
+    c = 300;
+
+    cout << "a변수의 값 : " << a << endl;
+    cout << "b변수의 값 : " << b << endl;
+    cout << "c변수의 값 : " << c << endl;
+
+    return 0;
+}
