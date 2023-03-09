2edde37e1c6e1e00e28f0940550cfca2b724b793
yodayeong <duekdud01@naver.com>
2023-03-09 14:03:36 +0900

    fix: std 개념 공부

--- C++/helloworld.cpp
+++ C++/helloworld.cpp
@@ -1,9 +1,9 @@
 #include <iostream> //헤더파일
 
-using namespace std;  //네임스페이스
+using namespace std;  //std에 정의되어 있는 함수들을 사용하겠다.
 
 int main(void) {
-    cout << "Hello World" << endl; //문자열 출력
+    cout << "Hello World" << endl; //문자열 출력 (cout 객체와 endl 객체)
 
     return 0;
 }
